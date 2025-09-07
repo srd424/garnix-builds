3b3efcb7f3ef5b73549a8194b350285df06a8dbc
Steve Dee <steved424@gmail.com>
2025-09-07 18:24:41 +0100

    Merge in gnucash54 build stuff

--- flake.nix
+++ flake.nix
@@ -3,17 +3,40 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs2305.url = "github:nixos/nixpkgs/nixos-23.05";
     tgt-glfs = {
       url = "git+https://codeberg.org/srd424/tgt-glfs-nix.git";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, tgt-glfs }: let
+  outputs = { self, nixpkgs, nixpkgs2305, tgt-glfs }: let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      pkgs2305 = nixpkgs2305.legacyPackages.x86_64-linux;
+
     in {
-      packages.x86_64-linux.tgt = tgt-glfs.packages.x86_64-linux.tgt;
-    };
+      packages.x86_64-linux.tgt-glfs = tgt-glfs.packages.x86_64-linux.tgt;
+
+      packages.x86_64-linux.gnucash54 =
+        pkgs2305.gnucash.overrideAttrs (prevAttrs: {
+          patches = (prevAttrs.patches or []) ++ [
+            ./gnucash54/python-env.patch
+           ];
+          cmakeFlags = [
+            "-DWITH_PYTHON=\"ON\""
+            "-DPYTHON_SYSCONFIG_BUILD=\"$out\""
+          ];
+          buildInputs = prevAttrs.buildInputs ++ (with pkgs2305; [
+            python3
+          ]);
+          postPatch = ''
+            substituteInPlace bindings/python/__init__.py \
+            --subst-var-by gnc_dbd_dir "${pkgs2305.libdbiDrivers}/lib/dbd" \
+            --subst-var-by gsettings_schema_dir ${pkgs2305.glib.makeSchemaPath "$out" "gnucash-${prevAttrs.version}"};
+          '';
+        });
+      packages.x86_64-linux.gnucash54-pymodule = pkgs.python3Packages.toPythonModule self.packages.x86_64-linux.gnucash54;
+  };
 }
 
 # vim: set ts=2 sw=2 et sta:
