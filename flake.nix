1fc71842496bdd9f7b4115f6c89bf8825b5387c1
Steve Dee <steved424@gmail.com>
2026-07-26 11:11:59 +0100

    Add nixpkgs 26.05 input, use it to build ssh-agent-switcher

--- flake.nix
+++ flake.nix
@@ -2,8 +2,9 @@
   description = "flake to build various useful packages on garnix";
 
   inputs = {
-    nixpkgs2511.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs2305.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs2511.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs2605.url = "github:nixos/nixpkgs/nixos-26.05";
     tgt-glfs = {
       url = "git+https://codeberg.org/srd424/tgt-glfs-nix.git";
       inputs.nixpkgs.follows = "nixpkgs2511";
@@ -27,7 +28,7 @@
     };
     ssh-agent-switcher = {
       url = "github:jmmv/ssh-agent-switcher?rev=9cf67475a143b70b5f5076eb1d3747cbac10220b";
-      inputs.nixpkgs.follows = "nixpkgs2511";
+      inputs.nixpkgs.follows = "nixpkgs2605";
     };
   };
 
