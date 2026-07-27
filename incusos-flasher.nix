e415b7cd4ee0b5ce7883859f75dbfa5af6181f46
Steve Dee <steved424@gmail.com>
2026-07-27 22:45:33 +0100

    flasher-tool: roll back to 202607072359 because of https://github.com/lxc/incus-os/issues/1227

--- incusos-flasher.nix
+++ incusos-flasher.nix
@@ -5,12 +5,12 @@
 }:
 
 let
-  version = "202607270148";
+  version = "202607072359";
   src = fetchFromGitHub {
     owner = "lxc";
     repo = "incus-os";
     rev = "${version}";
-    hash = "sha256-mSoMrdvmSfI0AKKIyg0nIpefjhByb84Lve4IRBbneg0=";
+    hash = "sha256-9MEkGbKlCVm6S9wVpF+Rtn8Ws5gst0eiqxP205isOC0=";
   };
 in
 buildGoModule {
@@ -20,7 +20,7 @@ buildGoModule {
   sourceRoot = "${src.name}/incus-osd";
   subPackages = [ "cmd/flasher-tool" ];
 
-  vendorHash = "sha256-mQFPsTAmuwJVnxH4DnLQHOsOOb2h4DSZRKpUXNB/doE=";
+  vendorHash = "sha256-PI5r//RzrBU7Ocy3I5T400S4dmV6ELy6BPnVvW5pYQw=";
 
   meta = with lib; {
     homepage = "https://github.com/lxc/incus-os";
