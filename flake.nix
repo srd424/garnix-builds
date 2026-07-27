5c4e862a939ef16ff431837ee6ba4885fa1e25e2
Steve Dee <steved424@gmail.com>
2026-07-27 22:04:03 +0100

    Build incusos flasher tool uing nixpkgs 26.05

--- flake.nix
+++ flake.nix
@@ -73,9 +73,9 @@
         });
 
       packages.x86_64-linux.incusos-flasher =
-            pkgs2511.callPackage ./incusos-flasher.nix {};
+            pkgs2605.callPackage ./incusos-flasher.nix {};
       packages.aarch64-linux.incusos-flasher =
-        nixpkgs2511.legacyPackages.aarch64-linux.callPackage ./incusos-flasher.nix {};
+        nixpkgs2605.legacyPackages.aarch64-linux.callPackage ./incusos-flasher.nix {};
 
       hydraJobs =
         let
